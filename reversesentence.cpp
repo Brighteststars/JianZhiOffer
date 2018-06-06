1f15f966dd26dff86313e9cf8ec2a64e16c0e7de
冲击之刃 <980770546@qq.com>
2018-06-07 00:33:35 +0800

    面试题58：翻转字符串
    2种实现

---
+++ reversesentence.cpp
@@ -0,0 +1,71 @@
+﻿#include <algorithm>>
+#include <iostream>
+#include <string>
+
+void Reverse(char* pBegin,char* pEnd)
+{
+    if(pBegin == nullptr || pEnd == nullptr)
+        return;
+
+
+
+    while(pBegin < pEnd)
+    {
+        char temp = *pBegin;
+        *pBegin = *pEnd;
+        *pEnd = temp;
+        pBegin++;
+        pEnd--;
+    }
+
+
+}
+
+char* ReverseSentence(char* pData)
+{
+    if(pData == nullptr)
+        return nullptr;
+    char* pBegin = pData;
+    char* pEnd = pData;
+    while(*pEnd != '\0')
+        ++pEnd;
+
+    --pEnd;
+    Reverse(pBegin,pEnd);
+
+    pBegin = pEnd = pData;
+    while(*pBegin != '\0')
+    {
+        if(*pBegin == ' ')
+        {
+            ++pBegin;
+            ++pEnd;
+        }
+        else if(*pEnd == ' ' || *pEnd == '\0')
+        {
+            Reverse(pBegin,--pEnd);
+            pBegin = ++pEnd;
+        }
+        else
+            ++pEnd;
+    }
+
+    return pData;
+}
+
+
+int main() {
+    string words;
+    while (getline(cin, words)) {
+        reverse(words.begin(), words.end()); //reverse()STL算法
+        unsigned i = 0, j = i;
+        while (i < words.size()) {
+            while (i < words.size() && words[i] != ' ') // words[i]不能解引用,i = words.size()
+                ++i;
+            reverse(words.begin() + j, words.begin() + i);
+            j = ++i;
+        }
+        cout << words << endl;
+    }
+    return 0;
+}
