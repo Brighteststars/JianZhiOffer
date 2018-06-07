21d69bf670c95988779e344093cc7032551117eb
冲击之刃 <980770546@qq.com>
2018-06-07 20:33:47 +0800

    面试题19：正则表达式匹配

---
+++ regex.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+    //如果*(pattern+1) == '*' && pattern 和 str第一个字符相等,有三种情况
+    //直接匹配下一个 || 保持原状态 || 忽略'*'
+
+    bool match(char* str, char* pattern)
+    {
+        if(str == nullptr || pattern == nullptr)
+            return false;
+        if(*str == '\0' && *pattern == '\0')
+            return true;
+        if(*str != '\0' && *pattern == '\0')
+            return false;
+        if(*(pattern+1) == '*')
+        {
+            if(*pattern == *str || (*pattern == '.' && *str != '\0'))
+                return match(str+1,pattern +2) || match(str+1,pattern) || match(str,pattern+2);
+            else
+                return match(str,pattern+2); //忽略‘*’
+        }
+        if(*pattern == *str || (*pattern == '.' && *str != '\0'))
+            return match(str+1,pattern+1);
+        return false;
+    }
+
+
+};
